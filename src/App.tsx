944531d2e29edb7c3d0333c1a4d7a772d1da3a66
Vũ Đại Minh <minhvd.1111@gmail.com>
2024-09-29 16:43:16 +0700

    testing flowise ai chatbot

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Typography } from "antd";
 import "./App.css";
 import PostList from "./components/PostList";
+import { BubbleChat } from 'flowise-embed-react'
 
 const { Title } = Typography;
 
@@ -9,6 +10,7 @@ const App = () => {
     <>
       <Title>Post Management</Title>
       <PostList />
+      <BubbleChat chatflowid="07fd5763-6559-4a45-95e5-c45e2defd80a" apiHost="http://localhost:3000" />
     </>
   );
 };
